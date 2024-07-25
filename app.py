853f8d71fb108d99bc0cb5b4f8f42b9a6797de2f
KARTHNA <nagakarthikmn0123@gmail.com>
2024-07-25 17:17:22 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, request, jsonify,render_template
+from flask import Flask, request, jsonify,render_template,send_from_directory
 import requests
 import json
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return send_from_directory(os.getcwd(),'index.html')
 
 # Endpoint to receive user input and process it
 @app.route('/process', methods=['POST'])
